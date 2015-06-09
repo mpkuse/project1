233911ed1af80ba57b1bbafef6ae0a8bd96f024b
mpkuse <mpkuse@ust.hk>
2015-06-09 17:33:52 +0800

    implemented gauss newton method

---
+++ gauss_newton.cpp
@@ -0,0 +1,3 @@
+The Gauss–Newton algorithm is a method used to solve non-linear least squares problems. It is a modification of Newton's method for finding a minimum of a function. Unlike Newton's method, the Gauss–Newton algorithm can only be used to minimize a sum of squared function values, but it has the advantage that second derivatives, which can be challenging to compute, are not required.
+Non-linear least squares problems arise for instance in non-linear regression, where parameters in a model are sought such that the model is in good agreement with available observations.
+The method is named after the mathematicians Carl Friedrich Gauss and Isaac Newton.
